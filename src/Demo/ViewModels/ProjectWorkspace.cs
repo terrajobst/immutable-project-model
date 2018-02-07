f3821e53fb2296d073d92fbcccedb7e3a1125d9c
Immo Landwerth <immo@landwerth.net>
2018-02-06 18:03:11 -0800

    Fix whitespace in usings

--- src/Demo/ViewModels/ProjectWorkspace.cs
+++ src/Demo/ViewModels/ProjectWorkspace.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+
 using Immutable.ProjectModel;
 
 namespace Demo.ViewModels
--- src/Demo/ViewModels/UndoRedoViewModel.cs
+++ src/Demo/ViewModels/UndoRedoViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Windows.Input;
+
 using Immutable.ProjectModel;
 
 namespace Demo.ViewModels
