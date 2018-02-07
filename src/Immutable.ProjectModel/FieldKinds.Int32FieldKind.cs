9bdaa8cb022697e099da1c138e443c04074f4739
Immo Landwerth <immo@landwerth.net>
2018-02-06 18:04:04 -0800

    Make Int32 a real field type

---
+++ src/Immutable.ProjectModel/FieldKinds.Int32FieldKind.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Immutable.ProjectModel
+{
+    public static partial class FieldKinds
+    {
+        private sealed class Int32FieldKind : FieldKind
+        {
+            public override Type Type => typeof(int);
+
+            public override string Format(object value)
+            {
+                var actualValue = (int)value;
+                return actualValue.ToString();
+            }
+
+            public override bool TryParse(string text, out object value)
+            {
+                if (text != null)
+                {
+                    if (int.TryParse(text, out var actualValue))
+                    {
+                        value = actualValue;
+                        return true;
+                    }
+                }
+
+                value = null;
+                return false;
+            }
+        }
+    }
+}
